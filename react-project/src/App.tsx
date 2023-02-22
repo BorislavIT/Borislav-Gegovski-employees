2b274850466037cb24a501b840bf698aad9d88ec
Borislav Gegovski <mutenroshiyo1@gmail.com>
2023-02-22 04:31:57 +0200

    Removed useless import

--- react-project/src/App.tsx
+++ react-project/src/App.tsx
@@ -1,4 +1,3 @@
-import { IntlProvider } from "react-intl";
 import { EmployeesData } from "./components/Employees/Employees";
 import "./styles/App.scss";
 import "primereact/resources/themes/lara-light-indigo/theme.css";
@@ -8,9 +7,7 @@ import "primeicons/primeicons.css";
 export default function App() {
   return (
     <div className="App">
-      <IntlProvider locale="en" defaultLocale="en">
-        <EmployeesData />
-      </IntlProvider>
+      <EmployeesData />
     </div>
   );
 }
